a018f8be679e9696921c2c25cf9d1101c5f4d2e6
jooYeon <conn0204@naver.com>
2023-08-09 20:13:59 +0900

    스위치 변경 시, 스타일 변경

--- TodoList/TodoList/TodoListTableCell.swift
+++ TodoList/TodoList/TodoListTableCell.swift
@@ -12,12 +12,21 @@ class TodoListTableCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var complitedSwitch: UISwitch!
     
+    @IBAction func changeStyle(_ sender: UISwitch) {
+        changeStyle(isOn: sender.isOn)
+    }
+    
+    
     func config(index: Int) {
         
         titleLabel.text = todoList[index].title
         complitedSwitch.isOn = todoList[index].isComplited
         
-        if !complitedSwitch.isOn {
+        changeStyle(isOn: todoList[index].isComplited)
+    }
+    
+    func changeStyle(isOn: Bool) {
+        if !isOn {
             contentView.backgroundColor = .white
             titleLabel.textColor = .black
         } else {
