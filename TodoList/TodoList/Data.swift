d7176d9dd1f15971a4271b34f4688ce5c309b146
jooYeon <conn0204@naver.com>
2023-08-10 23:59:32 +0900

    detail 뷰 스타일 설정

--- TodoList/TodoList/Data.swift
+++ TodoList/TodoList/Data.swift
@@ -16,12 +16,46 @@ struct TodoList {
 }
 
 var todoList: [TodoList] = [
-    TodoList(isComplited: true, title: "UILabel 공부하기", startDate: "0000-00-00", endDate: "1111-11-11", imageName: "cat"),
-    TodoList(isComplited: true, title: "UIButton 공부하기", startDate: "0000-00-00", endDate: "1111-11-11", imageName: "catcat22"),
-    TodoList(isComplited: true, title: "UISwitch 공부하기", startDate: "0000-00-00", endDate: "1111-11-11", imageName: "cat_day"),
-    TodoList(isComplited: false, title: "UINavgation 공부하기", startDate: "0000-00-00", endDate: "1111-11-11", imageName: "cat"),
-    TodoList(isComplited: false, title: "UITableViewl, UITableViewController 공부하기", startDate: "0000-00-00", endDate: "1111-11-11", imageName: "cat"),
-    TodoList(isComplited: false, title: "UITableCell 공부하기", startDate: "0000-00-00", endDate: "1111-11-11", imageName: "cat"),
-    TodoList(isComplited: false, title: "투두리스트 라벨 짤릴 때 어떻게 할지", startDate: "0000-00-00", endDate: "1111-11-11", imageName: "cat")
+    TodoList(isComplited: true,
+             title: "UILabel 공부하기",
+             startDate: "0000-00-00",
+             endDate: "2023-01-11",
+             imageName: "cat"),
+    
+    TodoList(isComplited: true,
+             title: "UIButton 공부하기",
+             startDate: "0000-00-00",
+             endDate: "2023-02-11",
+             imageName: "catcat22"),
+    
+    TodoList(isComplited: true,
+             title: "UISwitch 공부하기",
+             startDate: "0000-00-00",
+             endDate: "2023-03-11",
+             imageName: "cat_day"),
+    
+    TodoList(isComplited: false,
+             title: "UINavgation 공부하기",
+             startDate: "0000-00-00",
+             endDate: "2023-04-11",
+             imageName: "cat"),
+    
+    TodoList(isComplited: false,
+             title: "UITableViewl, UITableViewController 공부하기 열심히 공부하기 또 공부하기 마지막",
+             startDate: "0000-00-00",
+             endDate: "2023-05-11",
+             imageName: "cat"),
+    
+    TodoList(isComplited: false,
+             title: "UITableCell 공부하기",
+             startDate: "0000-00-00",
+             endDate: "2023-11-11",
+             imageName: "cat"),
+    
+    TodoList(isComplited: false,
+             title: "투두리스트 라벨 짤릴 때 어떻게 할지",
+             startDate: "0000-00-00",
+             endDate: "2023-12-11",
+             imageName: "cat")
 ]
 
--- TodoList/TodoList/TodoDetailViewController.swift
+++ TodoList/TodoList/TodoDetailViewController.swift
@@ -19,5 +19,47 @@ class TodoDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        config()
+    }
+    
+    func config() {
+        setupTitleLabel()
+        setupStartDateLabel()
+        setupEndDateLabel()
+    }
+    
+    func setupTitleLabel() {
+        titleLabel.text = todoList[index].title
+        titleLabel.backgroundColor = .systemPurple
+        titleLabel.textColor = .white
+        titleLabel.textAlignment = .center
+        
+        //한 줄 이상이 되면 그에 맞춰 동적으로 높이를 변경시켜줘야 하는 게 맞을 거 같은데...
+        titleLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    func setupStartDateLabel() {
+        startDateLabel.text = todoList[index].startDate
+        startDateLabel.textAlignment = .right
+    }
+    
+    func setupEndDateLabel() {
+        endDateLabel.text = todoList[index].endDate
+        endDateLabel.textAlignment = .right
+        
+        if !todoList[index].isComplited { checkEndDate() }
+    }
+    
+    func checkEndDate() {
+        let date = Date()
+        let endDateString = todoList[index].endDate
+        
+        let dateFormatter = DateFormatter()
+         dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        guard let endDate = dateFormatter.date(from: endDateString) else { return }
+
+        endDateLabel.textColor = date.compare(endDate) == .orderedDescending ? .red : .black
     }
 }
