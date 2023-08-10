11c7140965c4fb6223e670faf1c65d0ccc752e58
jooYeon <conn0204@naver.com>
2023-08-10 15:07:33 +0900

    텍스트필드를 추가해서, 시작날짜와 종료날짜도 입력 받기

--- TodoList/TodoList/TodoListViewController.swift
+++ TodoList/TodoList/TodoListViewController.swift
@@ -19,6 +19,22 @@ class TodoListViewController: UIViewController {
         
         insertListButton()
     }
+}
+
+extension TodoListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = todoListTableView.dequeueReusableCell(withIdentifier: "TodoListTableCell", for: indexPath) as! TodoListTableCell
+        cell.config(index: indexPath.row)
+        
+        return cell
+    }
+}
+
+extension TodoListViewController {
     
     func insertListButton() {
         let button = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(insertLits))
@@ -32,36 +48,41 @@ class TodoListViewController: UIViewController {
         let cancel = UIAlertAction(title: "취소", style: .cancel)
         
         let insert = UIAlertAction(title: "추가", style: .default) {_ in
-            if let title = alert.textFields?[0].text, !title.isEmpty {
-                todoList.append(TodoList(isComplited: false, title: title, startDate: "3333-33-33", endDate: "4444-44-44", imageName: "추가 디테일"))
-                //생각해 볼 것
-                //1) 일단 이 함수를 써서 갱신시켜도 되는지?
-                //2) viewDidLoad에서 메소드를 호출해도 되는지?
-                self.todoListTableView.reloadData()
-            }
+            guard let title = alert.textFields?[0].text, !title.isEmpty else { return }
+            guard let startDate = alert.textFields?[1].text, !startDate.isEmpty else { return }
+            guard let endDate = alert.textFields?[2].text, !endDate.isEmpty else { return }
+            
+            //원래라면 image도 입력받아야 하지만 일단 패스..
+            todoList.append(TodoList(isComplited: false,
+                                     title: title,
+                                     startDate: startDate,
+                                     endDate: endDate,
+                                     imageName: "cat"))
         }
+        
+        //생각해 볼 것
+        //1) 일단 이 함수를 써서 갱신시켜도 되는지?
+        self.todoListTableView.reloadData()
         
         alert.addAction(cancel)
         alert.addAction(insert)
         
-
+        
         alert.addTextField { (textFields) in
             textFields.keyboardType = .alphabet
+            textFields.placeholder = "제목"
+        }
+        
+        alert.addTextField { (textFields) in
+            textFields.keyboardType = .alphabet
+            textFields.placeholder = "시작 날짜"
+        }
+        
+        alert.addTextField { (textFields) in
+            textFields.keyboardType = .alphabet
+            textFields.placeholder = "종료 날짜"
         }
         
         present(alert, animated: false)
-    }
-}
-
-extension TodoListViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todoList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = todoListTableView.dequeueReusableCell(withIdentifier: "TodoListTableCell", for: indexPath) as! TodoListTableCell
-        cell.config(index: indexPath.row)
-        
-        return cell
     }
 }
